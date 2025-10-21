4508aeaa52d65654f877db962e05049d768e0548
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2025-10-21 14:09:51 +0530

    login controllers updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,49 +1,95 @@
-const bcrypt=  require ("bcrypt")
-const User = require("../models/userModel")
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
+const User = require("../models/userModel");
 
-const registerUser = async(req,res)=>{
-    const {name , email , password} = req.body;
-    try {
-        const isUserExist = await User.findOne({email})
-        if(isUserExist){
-            return res.status(400).json({
-                success :false,
-                message :"User already exist "
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(password , 10);
-        const user = new User({
-            name,
-            email,
-            password : hashedPassword
-        })
-
-        const userData = await user.save()
-
-        return res.status(201).json({
-            success :true,
-            message :"User registered successfully!!!",
-            data :userData
-        })
-
-
-
-
-    } catch (error) {
-        res.status(500).json({
-            success:false,
-            message:error.message 
-        })
-        
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const isUserExist = await User.findOne({ email });
+    if (isUserExist) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exist ",
+      });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const userData = await user.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully!!!",
+      data: userData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+const generateAccessToken = (user)=>{
+    const token = JWT.sign(user , process.env.SECRET_KEY, {expiresIn:"2h"})
+    return token;
 }
 
 
-const loginUser = (req,res,next)=>{
-
-}
 
 
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(501).json({
+        success: false,
+        message: "Incorrect Email or password",
+      });
+    }
 
-module.exports = {registerUser,loginUser}
+    //JWT signing
+
+    const isPassCorrect = await bcrypt.compare(password , user.password)
+    if(!isPassCorrect){
+        return res.status(500).json({
+            success: false,
+            message :"Incorrect Email or password "
+        })
+    }
+
+    const accessToken = generateAccessToken({
+        id: user._id,
+        email: user.email,
+    })
+    console.log();
+    
+
+
+    return res.status(200).json({
+        status:true,
+        message:"Login Successfull",
+        userData: user,
+        accessToken : accessToken,
+        tokenType:"Bearer"
+        
+    })
+
+
+
+  } catch (error) {
+    return res.status(500).json({
+        success : false,
+        message: error.message
+    })
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- server/models/permissionModel.js
+++ server/models/permissionModel.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose")
 
 const permissionSchema = new mongoose.Schema({
-    user_id : {
-        type: mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref :"User",
+    
+    
+    permission_name:{
+        type:String,
+        required:true
     },
-    permissions :[{
-        permission_name:String,
-        // User,Comment,Like,Post
-        permission_value:[Number],
-        // 0-->Create
-        // 1-->Read
-        // 2-->Update 
-        // 3-->Delete       
-    }]
+    is_default:{
+        type:Number,
+        default : 0
+    }
+    
 })
 
 const permissionModel = mongoose.model("Permission" , permissionSchema)
